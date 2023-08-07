82af1158ef1966098f2d44ac562e5bda492edeca
spomaria <spomariae@gmail.com>
2023-08-07 20:33:25 +0100

    My Second Commit

---
+++ javascripts/index.js
@@ -0,0 +1,53 @@
+console.log("javascript" + " " + "is cool");
+var myString = "new to javascript";
+console.log(myString);
+var x = 2;
+var y = 5;
+console.log("x = ", x, ", y = ", y);
+console.log("x + y = ", x + y);
+console.log("x - y = ", x - y);
+console.log("x * y = ", x * y);
+console.log("x / y = ", x / y);
+console.log("x % y = ", x % y);
+console.log("y % x = ", y % x);
+
+// This is how to increment in javascript
+let myVar =5;
+myVar += 1;
+console.log("myVar = ", myVar);
+// myVar + 1;
+// console.log("myVar = ", myVar);
+// myVar =+ 1;
+// console.log("myVar = ", myVar);
+// myVar ++;
+// console.log("myVar = ", myVar);
+// myVar = ++myVar;
+// console.log("myVar = ", myVar);
+myVar = myVar++;
+console.log("myVar = ", myVar);
+
+var my2Var =10;
+// my2Var -= 1;
+// console.log("my2Var = ", my2Var);
+// my2Var -1; //didn't work
+// console.log("my2Var = ", my2Var);
+// my2Var =- 1;
+// console.log("my2Var = ", my2Var);
+// my2Var --;
+// console.log("my2Var = ", my2Var);
+// my2Var = --my2Var;
+// console.log("my2Var = ", my2Var);
+my2Var = my2Var--; //didn't work
+console.log("my2Var = ", my2Var);
+
+var myDec = 2.5;
+// myDec = myDec + 5;
+// console.log("myDec = ", myDec);
+// myDec = myDec * 2.2;
+// console.log("myDec = ", myDec);
+myDec = myDec % 2;
+console.log("myDec = ", myDec);
+
+// for(i=0, i <10, i++);{
+//     console.log("i = ", i);
+// }
