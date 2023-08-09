b7544d98b6f660eed303555c27c3ea605a138f04
spomaria <spomariae@gmail.com>
2023-08-09 09:25:57 +0100

    My Fourth Commit

---
+++ javascripts/assignment_07_08_2023.js
@@ -0,0 +1,16 @@
+var x = 5;
+var y = 2;
+var numAdd = "x + y = ";
+var numDifference = "x - y = ";
+var numProduct = "x * y = ";
+var numDivision = "x / y = ";
+var numQuotient = "x % y = ";
+
+console.log("x = ", x);
+console.log("y = ", y);
+console.log(numAdd, x + y);
+console.log(numDifference, x - y);
+console.log(numProduct, x * y);
+console.log(numDivision, x / y);
+console.log(numQuotient, x % y);
+
